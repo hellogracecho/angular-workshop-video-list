a2521eda0c086f07e2a2e4d73e4e873f99b01254
Grace Cho <48530228+hellogracecho@users.noreply.github.com>
2020-04-15 11:44:18 -0700

    first commit

---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { MaterialModule } from '../ui/material/material.module';
+import { VideoDashboardComponent } from './video-dashboard/video-dashboard.component';
+import { VideoListComponent } from './video-list/video-list.component';
+import { VideoPlayerComponent } from './video-player/video-player.component';
+import { StatFiltersComponent } from './stat-filters/stat-filters.component';
+
+const routes: Routes = [
+  { path: '', component: VideoDashboardComponent }
+];
+
+
+@NgModule({
+  declarations: [
+    VideoDashboardComponent,
+    VideoListComponent,
+    VideoPlayerComponent,
+    StatFiltersComponent
+  ],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    RouterModule.forChild(routes)
+  ]
+})
+export class DashboardModule { }
---
+++ src/app/ui/material/material.module.ts
@@ -0,0 +1,71 @@
+import { NgModule } from '@angular/core';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatSelectModule } from "@angular/material/select";
+import { MatInputModule } from "@angular/material/input";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatRadioModule } from "@angular/material/radio";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
+import { MatSortModule } from "@angular/material/sort";
+import { MatTableModule } from "@angular/material/table";
+import { MatIconModule } from "@angular/material/icon";
+import { MatRippleModule, MatNativeDateModule } from "@angular/material/core";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatSlideToggleModule,
+    MatSortModule,
+    MatTableModule,
+    MatIconModule,
+    MatRippleModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatMenuModule,
+    MatTooltipModule,
+    MatPaginatorModule,
+    MatChipsModule,
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatListModule
+  ],
+  exports: [
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatSlideToggleModule,
+    MatSortModule,
+    MatTableModule,
+    MatIconModule,
+    MatRippleModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatMenuModule,
+    MatTooltipModule,
+    MatPaginatorModule,
+    MatChipsModule,
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatListModule
+  ]
+})
+export class MaterialModule { }
